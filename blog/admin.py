21d57ae0fb4c65185d6948b6abb82e1d53c51f7a
parto <zparto82@gmail.com>
2023-06-13 12:42:49 +0330

    add admin setting to blog app

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Category, Post, Comment
 
-# Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("name", "slug", "jpublish", "list_category" ,"is_active")
+    list_filter = (
+        "is_active", "jpublish"
+    )
+    search_fields = ("name", "description")
+    prepopulated_fields = {"slug": ("name",)}
+    ordering = [
+        "-is_active", "-jpublish"
+    ]
+
+    def list_category(self, obj):
+        return ", ".join(str(category) for category in obj.category.all())
+    list_category.short_description = "دسته‌بندی"
+
+admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
