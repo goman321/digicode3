8294909d6439c298e6b791a996a15cbceec98abf
parto <zparto82@gmail.com>
2023-06-13 12:35:07 +0330

    add apps and content editor to settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ckeditor',
+
+    'pages',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
