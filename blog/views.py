c8ad1b0cf0be201a1575eeec9635394fc5c3db0c
parto <zparto82@gmail.com>
2023-06-13 13:29:15 +0330

    debug errors

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import ListView
-from .models import BlogCategory, BlogPost
+from .models import Category, Post
 
 
 def show_post(request, slug):
-    result = get_object_or_404(BlogPost, slug=slug)
+    result = get_object_or_404(Post, slug=slug)
 
     return render(request, "pages/blog_single.html", context={"post": result})
