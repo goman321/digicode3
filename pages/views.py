18789701c80c69bdaeb5d36a24d6e048b6482c3c
parto <zparto82@gmail.com>
2023-06-13 12:39:31 +0330

    add views to pages app

--- pages/views.py
+++ pages/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.views.decorators.cache import cache_page
+from django.core.cache import cache
+from django.http.response import HttpResponse
 
-# Create your views here.
+
+def clear_cache(request):
+    try: 
+        cache.clear()
+        res = "ok"
+    except:
+        res = "err"
+    return HttpResponse(res)
+
+# @cache_page(60 * 15)
+def index(request):
+    return render(request, "pages/index.html")
+
+# @cache_page(60*15)
+def about_us(request):
+    return render(request, "pages/about-us.html")
+
+# @cache_page(60*15)
+def contact(request):
+    return render(request, "pages/contact.html")
