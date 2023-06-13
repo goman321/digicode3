df80c178386a2d63d0d8c7e4f27880320a236103
parto <zparto82@gmail.com>
2023-06-13 12:38:20 +0330

    add post view in blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView
+from .models import BlogCategory, BlogPost
 
-# Create your views here.
+
+def show_post(request, slug):
+    result = get_object_or_404(BlogPost, slug=slug)
+
+    return render(request, "pages/blog_single.html", context={"post": result})
