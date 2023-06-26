77de1e687a3ca091a70e83ddf4dced4d4be933a4
parto <zparto82@gmail.com>
2023-06-26 23:45:45 +0330

    change database setting

--- config/settings.py
+++ config/settings.py
@@ -85,10 +85,21 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'root',
+        'PASSWORD': 'UfD5ZECaV23NkQ7dex0WtKXU',
+        'HOST': 'luca.iran.liara.ir',
+        'PORT': '32281',
     }
 }
 
