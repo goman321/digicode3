bdd3a2d670ec3caf8ec347f82bf1b46d6294a229
parto <zparto82@gmail.com>
2023-06-27 00:04:47 +0330

    edit

--- config/settings.py
+++ config/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR/"static/"
+STATIC_ROOT = BASE_DIR/"static_files/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
